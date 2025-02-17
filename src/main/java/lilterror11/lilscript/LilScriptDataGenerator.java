91f30a1cd2acc3aa4952d7f47f039552ba40f9b2
LilTerror11 <Alexander.lilterror11@gmail.com>
2025-02-17 16:40:45 +0930

    Put on github (No Development)

---
+++ src/main/java/lilterror11/lilscript/LilScriptDataGenerator.java
@@ -0,0 +1,11 @@
+package lilterror11.lilscript;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LilScriptDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
